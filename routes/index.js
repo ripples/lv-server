c91350dfd3a443759c32f94beb78eaa167077327
Ryan Stanley <stanleyrya@gmail.com>
2016-04-08 01:47:17 -0400

    added a basic filescraper with @GaryPWhite

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var filescraper = require('../lib/filescraper');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('Hi!');
+});
+
+/* GET file structure. */
+router.get('/fs', function(req, res, next) {
+  var dirTree = ('.');
+
+  filescraper.diretoryTreeToObj(dirTree, function(err, tree) {
+      if(err) console.error(err);
+      res.send(JSON.stringify(tree));
+  });
 });
 
 module.exports = router;
