5fdca72be6b4d60768ea82c0d3be6e7ef64182d4
cheyans <cheyan.set@gmail.com>
2016-05-30 02:34:28 -0400

    add lecture route

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 FROM molecularplayground/node-js
-RUN apk add --no-cache bash
+RUN apk add bash
 
 COPY . /src
 
 WORKDIR /src
 
 RUN npm install
-
-RUN npm install -g node-inspector
 
 CMD ["npm", "run", "start-dev"]
 
--- routes/lectures.js
+++ routes/lectures.js
@@ -1,19 +1,68 @@
 "use strict";
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const http = require('http');
+
+const database = require('../lib/database');
+const logger = require('../lib/logger');
+
+let httpAgent = new http.Agent({keepAlive: true});
 
 // return a list of every lecture the user can view
-router.post('/', function(req, res, next) {
-  var classIds = req.user.classIds;
-  // TODO using classnames, find a list of lectures that the user is in
+router.post('/', (req, res, next) => {
+  let courseId = parseInt(req.body.courseId);
+  let courseName = req.body.courseName;
 
-  var fakeData = [
-    { classname : 'COMPSCI 497', date : '10/14/15' },
-    { classname : 'COMPSCI 326', date : '10/13/15' },
-    { classname : 'COMPSCI 497', date : '10/11/15' }
-  ];
-  res.send(fakeData);
+  let options = {
+    hostname: "lv-media",
+    port: process.env.MEDIA_SERVER_PORT,
+    path: `${process.env.SEMESTER}/${courseName}`,
+    agent: httpAgent
+  };
+
+  let mediaRequest = new Promise((resolve, reject) => {
+    let request = http.get(options, res => {
+      let data = "";
+
+      res.on('data', chunk => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        resolve(JSON.parse(data));
+      });
+    });
+    request.setTimeout(500, () => {
+      reject(`Request to lv-media/${options.path}:${options.port}
+      timed out for course ${courseName} id: ${courseId}`);
+    });
+  });
+
+  let metaDataRequest = new Promise((resolve, reject) => {
+    database.getCourseMetaData(courseId, (err, result) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(result);
+    });
+  });
+
+  Promise.all([mediaRequest, metaDataRequest]).then(values => {
+    let mediaResponse = values[0];
+    let dbResponse = values[1];
+    let response = {
+      lectures: mediaResponse,
+      description: dbResponse.course_description,
+      prof: `${dbResponse.prof_fname} ${dbResponse.prof_lname}`,
+      profEmail: dbResponse.prof_email,
+      startDtm: dbResponse.start_dtm,
+      endDtm: dbResponse.end_dtm
+    };
+    res.send(response);
+  }, reason => {
+    logger.error(reason);
+  });
 });
 
 module.exports = router;
