d34f62a8ff0a54ffd84ae7c5d7259811a712a1e0
Cheyan Setayesh <cheyan.set@gmail.com>
2017-02-02 19:49:11 -0500

    only invite users on production

--- server/routes/internal/users.js
+++ server/routes/internal/users.js
@@ -13,17 +13,18 @@ const logger = require("../../libs/logger.js").logger;
 
 router.post("/invite", (req, res, next) => {
   const emails = req.body.emails;
-  if (process.env.NODE_ENV === "development") {
-    co(function* () {
-      yield emails.map(function* (email) {
-        const id = (yield database.insertResetIdForEmail(email)).insertId;
-        const token = auth.generateEmailJwt(email, id);
-        yield mailer.sendInviteReset(email, token);
-        logger.info(`Invited ${email}`);
-        res.send({message: "Emails are being sent"});
-      })
-    }).catch(next);
+  if (process.env.NODE_ENV !== "production") {
+    return res.send({message: "Emails only sent in production mode"})
   }
+  co(function* () {
+    yield emails.map(function* (email) {
+      const id = (yield database.insertResetIdForEmail(email)).insertId;
+      const token = auth.generateEmailJwt(email, id);
+      yield mailer.sendInviteReset(email, token);
+      logger.info(`Invited ${email}`);
+      res.send({message: "success"});
+    })
+  }).catch(next);
 });
 
 module.exports = router;
